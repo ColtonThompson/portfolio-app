e2d9d41e9388c017caa230110601671be972cbed
Colton Thompson <colton.thompson@live.com>
2023-10-05 14:46:56 -0600

    Update: Projects page is now separated by type

--- src/app/components/ProjectList.tsx
+++ src/app/components/ProjectList.tsx
@@ -12,22 +12,38 @@ interface ProjectItem {
 }
 
 export default function ProjectsList() {
-
   return(
-      <div className="container mx-auto">
-        <div className="flex flex-wrap justify-center items-center">
-          <div className="grid grid-rows-9 grid-cols-4 gap-2">
-            <div><InfoBox id={1} name="IronmanGuide" description="Guide on how to play as an IronMan in Old School RuneScape." repositoryURL="https://github.com/ColtonThompson/IronmanGuide"/></div>
-            <div><InfoBox id={1} name="ModGrabber" description="Tool to download mod music from ModArchive.org." repositoryURL="https://github.com/ColtonThompson/ModGrabber"/></div>
-            <div><InfoBox id={1} name="Scraper" description="Web scraper to pull data from a list of members from a forum." repositoryURL="https://github.com/ColtonThompson/Scraper"/></div>
-            <div><InfoBox id={1} name="ScarySuperior" description="My first ReactJS project. Creates a table view for JSON data." repositoryURL="https://github.com/ColtonThompson/ScarySuperior"/></div>
-            <div><InfoBox id={1} name="Relleka Web" description="RuneScape fansite written in PHP/HTML/CSS." repositoryURL="https://github.com/ColtonThompson/Relleka"/></div>
-            <div><InfoBox id={1} name="AuthenticationSystem" description="School project to create an authentication system in Java." repositoryURL="https://github.com/ColtonThompson/AuthenticationSystem"/></div>
-            <div><InfoBox id={1} name="Astra" description="2D orthographic video-game prototype with a basic tile system using Slick2D." repositoryURL="https://github.com/Nightleaf/Astra"/></div>
-            <div><InfoBox id={1} name="Cepheus" description="2D space prototype built in Unity" repositoryURL="https://github.com/Nightleaf/Cepheus"/></div>
-          </div>
+  <div>
+    <h3 className="text-white py-4">Websites</h3>
+    <div className="container mx-auto">
+      <div className="flex flex-wrap justify-center items-center">  
+        <div className="grid grid-rows-9 grid-cols-4 gap-2">
+          <div><InfoBox id={1} name="IronmanGuide" description="Guide on how to play as an IronMan in Old School RuneScape." repositoryURL="https://github.com/ColtonThompson/IronmanGuide"/></div>
+          <div><InfoBox id={1} name="ScarySuperior" description="My first ReactJS project. Creates a table view for JSON data." repositoryURL="https://github.com/ColtonThompson/ScarySuperior"/></div>
+          <div><InfoBox id={1} name="Relleka Web" description="RuneScape fansite written in PHP/HTML/CSS." repositoryURL="https://github.com/ColtonThompson/Relleka"/></div>
         </div>
       </div>
+    </div>
+    <h3 className="text-white py-4">Video Games</h3>
+    <div className="container mx-auto">
+      <div className="flex flex-wrap justify-center items-center">  
+        <div className="grid grid-rows-9 grid-cols-4 gap-2">
+          <div><InfoBox id={1} name="Astra" description="2D orthographic video-game prototype with a basic tile system using Slick2D." repositoryURL="https://github.com/Nightleaf/Astra"/></div>
+          <div><InfoBox id={1} name="Cepheus" description="2D space prototype built in Unity" repositoryURL="https://github.com/Nightleaf/Cepheus"/></div>
+        </div>
+      </div>
+    </div>
+    <h3 className="text-white py-4">Desktop Applications</h3>
+    <div className="container mx-auto">
+      <div className="flex flex-wrap justify-center items-center">  
+        <div className="grid grid-rows-9 grid-cols-4 gap-2">
+          <div><InfoBox id={1} name="ModGrabber" description="Tool to download mod music from ModArchive.org." repositoryURL="https://github.com/ColtonThompson/ModGrabber"/></div>
+          <div><InfoBox id={1} name="Scraper" description="Web scraper to pull data from a list of members from a forum." repositoryURL="https://github.com/ColtonThompson/Scraper"/></div>
+          <div><InfoBox id={1} name="AuthenticationSystem" description="School project to create an authentication system in Java." repositoryURL="https://github.com/ColtonThompson/AuthenticationSystem"/></div>
+        </div>
+      </div>
+    </div>        
+  </div>
   );
 }
 
