c350a5df392e305405ae8d785677e52194f4db98
Colton Thompson <colton.thompson@live.com>
2023-12-13 11:41:40 -0700

    More Style Improvements!
    More work has been done to uplift the intro/projects pages.
    
    I removed the ABOUT ME module to be reworked in the next update.

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,49 +1,36 @@
 export default function About() {
     return (
         <div>
-            <div className="container flex justify-center items-center mx-auto">
-
-                <h2 className="pr-4 text-3xl font-bold text-slate-300">ABOUT ME</h2>
-                <div className="h-[250px] min-h-[1em] w-px self-stretch bg-gradient-to-tr from-transparent via-neutral-500 to-transparent opacity-20 dark:opacity-100"></div>
-                <p className="pl-6 text-slate-300">
-                    Recent B.S. graduate with a major in computer science and software development with personal experience in developing desktop & web-based applications.
-                </p>
-                <p className="pl-6 text-slate-300">
-                    I am also a content creator, my gaming YouTube channel (<a href="https://youtube.com/@Nightleaf" target="_blank">@Nightleaf</a>) was started on April 1st, 2023 and gained over 25,000 views by August 1st, 2023.
-                    I use the Adobe Creative Suite to create the artwork and illustrations, I edit my videos using Davinci Resolve but have also used Adobe Premiere in the past.
-                </p>
-                <p className="pl-6 text-slate-300">
-                    My goal is to work in the games industry! I am currently learning how to use Godot to build 2D projects, soon you will see those projects visible in <a href="/projects">My Projects</a> on this site.
-                </p>
-            </div>
             <div className="container justify-center items-center mx-auto">
-                <h2 className="text-4xl font-bold text-center text-slate-300">EDUCATION</h2>
-                <p className="pl-6 text-center text-slate-300">
+                <h2 className="text-4xl font-bold text-center text-gray-200">EDUCATION</h2>
+                <br/>
+                <p className="pl-6 text-center text-gray-200">Bachelor of Science - Major: Computer Science</p>
+                <p className="pl-6 text-center text-slate-400">
                     Graduated Cum Laude with an overall GPA of 3.65 at Southern New Hampshire University on May 1st, 2021.
                     <br />
                     <br />
-                    <h2 className="text-4xl font-bold text-center text-slate-300">SKILLS</h2>
+                    <h2 className="text-4xl font-bold text-center text-gray-200">SKILLS</h2>
 
                 </p>
                 <div className="flex justify-center items-center">
                     <table className="border-separate border-spacing-2 border border-slate-500 content-center">
                         <tbody>
                             <tr>
-                                <td className="border border-slate-700 text-slate-300">JAVA</td>
-                                <td className="border border-slate-700 text-slate-300">JAVASCRIPT</td>
-                                <td className="border border-slate-700 text-slate-300">PHP</td>
-                                <td className="border border-slate-700 text-slate-300">HTML</td>
-                                <td className="border border-slate-700 text-slate-300">CSS</td>
-                                <td className="border border-slate-700 text-slate-300">SPRINGBOOT</td>
-                                <td className="border border-slate-700 text-slate-300">REACT.JS</td>
-                                <td className="border border-slate-700 text-slate-300">TAILWIND</td>
-                                <td className="border border-slate-700 text-slate-300">MYSQL</td>
+                                <td className="border border-slate-700 text-slate-400">JAVA</td>
+                                <td className="border border-slate-700 text-slate-400">JAVASCRIPT</td>
+                                <td className="border border-slate-700 text-slate-400">PHP</td>
+                                <td className="border border-slate-700 text-slate-400">HTML</td>
+                                <td className="border border-slate-700 text-slate-400">CSS</td>
+                                <td className="border border-slate-700 text-slate-400">SPRINGBOOT</td>
+                                <td className="border border-slate-700 text-slate-400">REACT.JS</td>
+                                <td className="border border-slate-700 text-slate-400">TAILWIND</td>
+                                <td className="border border-slate-700 text-slate-400">MYSQL</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
-
+            <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
         </div>
     );
 }
--- src/app/components/Intro.tsx
+++ src/app/components/Intro.tsx
@@ -1,17 +1,21 @@
 
+'use client'
+
 import Image from "next/image"
+import { useRouter } from 'next/navigation'
 
 export default function Intro() {
+    const router = useRouter()
     return (
         <div className="container px-4 mx-auto">
-            <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
-                <div className="lg:px-4 lg:mt-12 ">
+            <div className="lg:space-x-3 lg:flex lg:flex-row item-center lg:-mx-2 flex flex-col-reverse text-center lg:text-left">
+                <div className="lg:px-2 lg:mt-10 ">
                     <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
                         Hey, I&apos;m Colton.
                     </h1>
                     <div className="mt-6 text-slate-400">
-                        <p className="mb-4">
-                        I&apos;m a software engineer from Utah in the United States. I like to create a wide variety of projects from desktop apps to websites to video games!
+                        <p className="mr-36 ml-36">
+                        I&apos;m a software engineer from Utah! I am a self taught software engineer that loves to create a myriad of projects ranging from single page web applications to video games.
                         </p>
                     </div>
                 </div>
@@ -24,7 +28,12 @@ export default function Intro() {
                         height={190}
                     />
                 </div>
+                <br/>
+
             </div>
+            <button className="item-center mr-32 ml-32 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-lg" onClick={() => router.push("/projects")}>
+                    View My Projects
+            </button>
             <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent opacity-25 dark:opacity-100" />
         </div>
     );
